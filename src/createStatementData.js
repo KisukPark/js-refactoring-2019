a5486375b7697d69a76892f2aaa68c397140615e
Gyehong Park <gyehongpark@gmail.com>
2019-03-19 07:33:02 +0900

    Replace Conditional with Polymorphism (272)

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -8,10 +8,7 @@ class PerformanceCalculator {
         let result = 0;
         switch (this.play.type) {
             case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
+                throw 'bad thing';
                 break;
             case "comedy":
                 result = 30000;
@@ -45,6 +42,14 @@ function createPerformanceCalculator(aPerformance, aPlay) {
 }
 
 class TragedyCalculator extends PerformanceCalculator{
+    get amount() {
+        let result = 0;
+        result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result;
+    }
 }
 
 class ComedyCalculator extends PerformanceCalculator {
