83dadb694802ed50859b17f150f555f094c23fb3
Gyehong Park <gyehongpark@gmail.com>
2019-03-19 06:36:28 +0900

    Moving Function into the Calculator

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -3,6 +3,28 @@ class PerformanceCalculator {
         this.performance = aPerformance;
         this.play = play;
     }
+
+    get amount() {
+        let result = 0;
+        switch (this.play.type) {
+            case "tragedy":
+                result = 40000;
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (this.performance.audience > 20) {
+                    result += 10000 + 500 * (this.performance.audience - 20);
+                }
+                result += 300 * this.performance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${this.play.type}`);
+        }
+        return result;
+    }
 }
 
 module.exports = function createStatementData(invoice, plays) {
@@ -43,26 +65,6 @@ module.exports = function createStatementData(invoice, plays) {
     }
 
     function amountFor(aPerformance) {
-        let result = 0;
-        let play = aPerformance.play;
-
-        switch (play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
-        return result;
+        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
     }
 }
