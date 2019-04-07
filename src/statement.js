34bb28eea7293acf4c899217d166807052294a8b
Ian Kim <holy.kss@gmail.com>
2019-04-07 22:27:32 +0900

    add intermediate data statementData

--- src/statement.js
+++ src/statement.js
@@ -1,8 +1,10 @@
 function statement (invoice, plays) {
-    return renderPlainText(invoice, plays);
+    const statementData = {};
+
+    return renderPlainText(statementData, invoice, plays);
 }
 
-function renderPlainText(invoice, plays) {
+function renderPlainText(data, invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
 
