ac8923bfc21bdd7b714addb737d7da645341d145
Ian Kim <holy.kss@gmail.com>
2019-04-07 22:24:16 +0900

    extract method renderPlainText

--- src/statement.js
+++ src/statement.js
@@ -1,4 +1,8 @@
 function statement (invoice, plays) {
+    return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
 
