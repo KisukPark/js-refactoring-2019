bf01cfd5843cfd058be313f9023bee7f3bed5bc0
Ian Kim <holy.kss@gmail.com>
2019-04-07 22:32:31 +0900

    enrichPerformance

--- src/statement.js
+++ src/statement.js
@@ -1,7 +1,14 @@
 function statement (invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances;
+
+    statementData.performances = invoice.performances.map(enrichPerformance);
+
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+
+        return result;
+    }
 
     return renderPlainText(statementData, plays);
 }
