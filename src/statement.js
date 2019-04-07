5ca928eccc27cad13572e042134e8efa49f76eb4
Ian Kim <holy.kss@gmail.com>
2019-04-07 22:45:26 +0900

    replace loop with pipeline - totalAmount

--- src/statement.js
+++ src/statement.js
@@ -50,12 +50,7 @@ function statement (invoice, plays) {
     }
 
     function totalAmount(data) {
-        let result = 0;
-
-        for (let perf of data.performances) {
-            result += perf.amount;
-        }
-        return result;
+        return data.performances.reduce((total, p) => total + p.amount, 0);
     }
 
     function totalVolumeCredits(data) {
