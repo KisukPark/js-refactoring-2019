1ba90b3f9bcb07db4016438650de725bb028edc6
yuaming <miennes3@gmail.com>
2019-04-09 23:04:30 +0900

    Add volumnCredits property in PerformanceCalculator

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -31,6 +31,14 @@ class PerformanceCalculator {
         }
         return result;
     }
+
+    get volumeCredits() {
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
+        return result;
+    }
 }
 
 function createStatementData(invoice, plays) {
@@ -45,20 +53,12 @@ function createStatementData(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
         result.amount = calculator.amount;
-        result.volumeCredits = volumeCreditsFor(result);
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID]
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
     }
 
     function totalAmount(data) {
