9be8d9f0aab29193cc9f68342837a5d0f173fd95
yuaming <miennes3@gmail.com>
2019-04-09 23:17:17 +0900

    Remove dead codes

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -5,31 +5,7 @@ class PerformanceCalculator {
     }
 
     get amount() {
-        let result = 0;
-        let play = this.performance.play;
-
-        switch (this.play.type) {
-            case "tragedy":
-                result = 40000;
-
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-
-                break;
-            case "comedy":
-                result = 30000;
-
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
-        return result;
+        throw new Error(`unknown type: ${this.play.type}`);
     }
 
     get volumeCredits() {
@@ -69,9 +45,8 @@ function createPerformanceCalculator(aPerformance, aPlay) {
     switch (aPlay.type) {
         case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
         case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+        default: return new PerformanceCalculator(aPerformance, aPlay);
     }
-
-    return new PerformanceCalculator(aPerformance, aPlay);
 }
 
 function createStatementData(invoice, plays) {
