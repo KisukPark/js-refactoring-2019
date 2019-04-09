34f99cf08f2cac8e00b7ddf5d0426324af18f564
yuaming <miennes3@gmail.com>
2019-04-09 23:16:04 +0900

    Add volumeCredits in ComedyCalculator

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -33,11 +33,7 @@ class PerformanceCalculator {
     }
 
     get volumeCredits() {
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
-        return result;
+        return Math.max(this.performance.audience - 30, 0);
     }
 }
 
@@ -62,6 +58,10 @@ class ComedyCalculator extends PerformanceCalculator {
 
         result += 300 * this.performance.audience;
         return result;
+    }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
     }
 }
 
