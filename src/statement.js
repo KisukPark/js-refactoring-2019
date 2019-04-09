e4238da1ad369ff6b959fbfa31cd943ec70f91b6
Ian Kim <holy.kss@gmail.com>
2019-04-09 23:17:16 +0900

    Ready to Part 4
    Splitting the phases of calculation and formatting

--- src/statement.js
+++ src/statement.js
@@ -1,7 +1,7 @@
 function statement (invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
-
     for (let perf of invoice.performances) {
+
         // print line for this order
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
@@ -10,45 +10,15 @@ function statement (invoice, plays) {
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
-    function totalAmount() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += amountFor(perf);
-        }
-        return result;
-    }
-
-    function totalVolumeCredits() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += volumeCreditsFor(perf);
-        }
-        return result;
-    }
-
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US",
-            { style: "currency", currency: "USD",
-                minimumFractionDigits: 2 }).format(aNumber/100);
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        // add volume credits
-        result += Math.max(aPerformance.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-        return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
+    function playFor(perf) {
+        return plays[perf.playID]
     }
 
     function amountFor(aPerformance) {
         let result = 0;
+        let play = playFor(aPerformance);
 
-        switch (playFor(aPerformance).type) {
+        switch (play.type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -63,9 +33,42 @@ function statement (invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+                throw new Error(`unknown type: ${play.type}`);
         }
+        return result;
+    }
 
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US",
+            {
+                style: "currency", currency: "USD",
+                minimumFractionDigits: 2
+            }).format(aNumber / 100);
+    }
+
+    function totalVolumeCredits() {
+        let result = 0;
+
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
+    }
+
+    function totalAmount() {
+        let result = 0;
+
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
         return result;
     }
 }
