986266204d756c1486e3290679a6bb1043f2ddba
doojin kim <doojin88@gmail.com>
2019-04-11 19:41:37 +0900

    usd: 변수 aNumber의 수치 단위를 dollar 에서 cent 로 변경.

--- src/statement.js
+++ src/statement.js
@@ -8,17 +8,17 @@ function statement (invoice, plays) {
         volumeCredits += volumeCreditsFor(perf);
 
         // print line for this order
-        result += `  ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${usd(totalAmount/100)}\n`;
+    result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US",
             { style: "currency", currency: "USD",
-                minimumFractionDigits: 2 }).format(aNumber);
+                minimumFractionDigits: 2 }).format(aNumber/100);
     }
 
     function volumeCreditsFor(aPerformance) {
