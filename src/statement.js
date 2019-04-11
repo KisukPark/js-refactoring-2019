e7e136b13227571ce1cee35c98560bd86f420a25
doojin kim <doojin88@gmail.com>
2019-04-11 19:47:28 +0900

    totalVolumeCreidts: inline volumeCredits variable.

--- src/statement.js
+++ src/statement.js
@@ -8,10 +8,8 @@ function statement (invoice, plays) {
         totalAmount += amountFor(perf);
     }
 
-    let volumeCredits = totalVolumeCredits();
-
     result += `Amount owed is ${usd(totalAmount)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
     function totalVolumeCredits() {
