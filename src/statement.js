b95839af7f91af0ca26cbf555b7c99218c4285a1
doojin kim <doojin88@gmail.com>
2019-04-11 19:58:20 +0900

    totalAmount: rename function name from appleSauce to totalAmount

--- src/statement.js
+++ src/statement.js
@@ -6,11 +6,11 @@ function statement (invoice, plays) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
 
-    result += `Amount owed is ${usd(appleSauce())}\n`;
+    result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
-    function appleSauce() {
+    function totalAmount() {
         let result = 0;
         for (let perf of invoice.performances) {
             result += amountFor(perf);
