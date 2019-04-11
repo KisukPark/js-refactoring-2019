3db5bb0651871bb73756ba50475df1ab9e354a60
doojin kim <doojin88@gmail.com>
2019-04-11 19:44:35 +0900

    totalVolumeCredits: volumeCredits variable 을 for 코드 위치로 옮긴다.

--- src/statement.js
+++ src/statement.js
@@ -1,6 +1,5 @@
 function statement (invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
@@ -9,6 +8,7 @@ function statement (invoice, plays) {
         totalAmount += amountFor(perf);
     }
 
+    let volumeCredits = 0;
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
     }
