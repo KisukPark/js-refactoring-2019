d6a42248a6d43b8894bf219fb738152713f9d350
doojin kim <doojin88@gmail.com>
2019-04-11 18:47:07 +0900

    amountFor: delete play parameter.

--- src/statement.js
+++ src/statement.js
@@ -7,7 +7,7 @@ function statement (invoice, plays) {
             minimumFractionDigits: 2 }).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf, playFor(perf));
+        let thisAmount = amountFor(perf);
 
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -26,7 +26,7 @@ function statement (invoice, plays) {
         return plays[aPerformance.playID];
     }
 
-    function amountFor(aPerformance, play) {
+    function amountFor(aPerformance) {
         let result = 0;
 
         switch (playFor(aPerformance).type) {
