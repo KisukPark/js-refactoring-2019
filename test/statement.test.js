0bb7ebacbbfcfec5a05944ba4352266dbf72ff0b
kisukpark <kisuk0521@gmail.com>
2019-04-12 03:37:43 +0900

    Add test 'x'

--- test/statement.test.js
+++ test/statement.test.js
@@ -2,5 +2,10 @@ const assert = require('assert');
 const statement = require('../src/statement');
 
 describe('statement', () => {
-
+  it('x', () => {
+    let invoice;
+    let plays;
+    const result = statement(invoice, plays);
+    assert.strictEqual(result, "");
+  });
 });
