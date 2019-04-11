32e0158092e750c401822dbd01b78658d11427c8
doojin kim <doojin88@gmail.com>
2019-04-11 19:39:19 +0900

    rename function format to usd.

--- src/statement.js
+++ src/statement.js
@@ -8,14 +8,14 @@ function statement (invoice, plays) {
         volumeCredits += volumeCreditsFor(perf);
 
         // print line for this order
-        result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `Amount owed is ${usd(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 
-    function format(aNumber) {
+    function usd(aNumber) {
         return new Intl.NumberFormat("en-US",
             { style: "currency", currency: "USD",
                 minimumFractionDigits: 2 }).format(aNumber);
