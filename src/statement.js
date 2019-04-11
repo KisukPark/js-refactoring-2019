be40bf7903c9074c5ca5d120700ce23aabc27b2e
doojin kim <doojin88@gmail.com>
2019-04-11 19:43:25 +0900

    totalVolumeCredits: refactoring 을 위해 for loop를 분리한다.

--- src/statement.js
+++ src/statement.js
@@ -4,13 +4,15 @@ function statement (invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
-
-        volumeCredits += volumeCreditsFor(perf);
-
         // print line for this order
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
+
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
+
     result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
