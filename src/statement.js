3545f9735a3f6b121f4451728a0d5a5248ad4359
doojin kim <doojin88@gmail.com>
2019-04-11 18:40:40 +0900

    amountFor: rename perf to aPerformance.

--- src/statement.js
+++ src/statement.js
@@ -22,22 +22,22 @@ function statement (invoice, plays) {
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 
-    function amountFor(perf, play) {
+    function amountFor(aPerformance, play) {
         let result = 0;
 
         switch (play.type) {
             case "tragedy":
                 result = 40000;
-                if (perf.audience > 30) {
-                    result += 1000 * (perf.audience - 30);
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
                 }
                 break;
             case "comedy":
                 result = 30000;
-                if (perf.audience > 20) {
-                    result += 10000 + 500 * (perf.audience - 20);
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
                 }
-                result += 300 * perf.audience;
+                result += 300 * aPerformance.audience;
                 break;
             default:
                 throw new Error(`unknown type: ${play.type}`);
