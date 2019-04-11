c2d63f5a02e9b3fa87faf0202142591ea0c8ee34
doojin kim <doojin88@gmail.com>
2019-04-11 19:46:35 +0900

    totalVolumeCredits: rename local variable volumeCredits to result.

--- src/statement.js
+++ src/statement.js
@@ -15,11 +15,11 @@ function statement (invoice, plays) {
     return result;
 
     function totalVolumeCredits() {
-        let volumeCredits = 0;
+        let result = 0;
         for (let perf of invoice.performances) {
-            volumeCredits += volumeCreditsFor(perf);
+            result += volumeCreditsFor(perf);
         }
-        return volumeCredits;
+        return result;
     }
 
     function usd(aNumber) {
