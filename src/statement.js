894d57a72830e8eb1f258c9908597e6ac7ef353b
doojin kim <doojin88@gmail.com>
2019-04-11 18:39:37 +0900

    amountFor: rename thisAmount to result.

--- src/statement.js
+++ src/statement.js
@@ -23,27 +23,27 @@ function statement (invoice, plays) {
     return result;
 
     function amountFor(perf, play) {
-        let thisAmount = 0;
+        let result = 0;
 
         switch (play.type) {
             case "tragedy":
-                thisAmount = 40000;
+                result = 40000;
                 if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
+                    result += 1000 * (perf.audience - 30);
                 }
                 break;
             case "comedy":
-                thisAmount = 30000;
+                result = 30000;
                 if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                    result += 10000 + 500 * (perf.audience - 20);
                 }
-                thisAmount += 300 * perf.audience;
+                result += 300 * perf.audience;
                 break;
             default:
                 throw new Error(`unknown type: ${play.type}`);
         }
 
-        return thisAmount;
+        return result;
     }
 }
 
