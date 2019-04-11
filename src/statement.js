3aa41b0f1ff22b6e771523e9130dadefce6307c3
doojin kim <doojin88@gmail.com>
2019-04-11 19:56:14 +0900

    totalAmount: 임시 함수 appleSauce 추출.

--- src/statement.js
+++ src/statement.js
@@ -6,14 +6,19 @@ function statement (invoice, plays) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
 
-    let totalAmount = 0;
-    for (let perf of invoice.performances) {
-        totalAmount += amountFor(perf);
-    }
+    let totalAmount = appleSauce();
 
     result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
+
+    function appleSauce() {
+        let totalAmount = 0;
+        for (let perf of invoice.performances) {
+            totalAmount += amountFor(perf);
+        }
+        return totalAmount;
+    }
 
     function totalVolumeCredits() {
         let result = 0;
