d91d16ec4b1cabb9c7fe88bf0c7a2a2d76cdd9e8
kisukpark <kisuk0521@gmail.com>
2019-04-12 03:47:22 +0900

    Until First Switch: Add another characterization test

--- test/statement.test.js
+++ test/statement.test.js
@@ -13,4 +13,21 @@ describe('statement', () => {
       "Amount owed is $0.00\n" +
       "You earned 0 credits\n");
   });
+
+  it('to first switch', () => {
+    let invoice = {
+      customer: 'BigCo',
+      performances: [
+        {
+          playID: 'hamlet',
+          audience: 20
+        },
+      ]
+    };
+    let plays = {
+      hamlet: { name: 'Hamlet', type: 'tragedy' }
+    };
+    const result = statement(invoice, plays);
+    assert.strictEqual(result, "");
+  });
 });
